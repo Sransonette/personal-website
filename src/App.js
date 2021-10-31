f5060992b5fb2c2f5d63e7b4dcb63d5d709c836b
Sean <Sransonette@gmail.com>
2021-10-30 19:15:27 -0500

    add background color and size scss to topbar.scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import Topbar from './components/topbar/topbar';
 
 
 function App() {
--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import "./topbar.scss"
 
 const Topbar = () => {
     return (
-        <div>
+        <div className="topbar">
 
         </div>
     );
