3a3413a2d99c487d00ac1c0d30a80add4e275b8c
Sean <Sransonette@gmail.com>
2021-11-29 23:00:19 -0600

    using modal-ui/icons I made a modal for my email in topbar

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./topbar.scss"
+import { Person, Mail } from "@material-ui/icons"
 
 const Topbar = () => {
     return (
@@ -8,7 +9,8 @@ const Topbar = () => {
                 <div className="left">
                     <a href="#intro" className="logo">Sean.</a>
                     <div className="itemContainer">
-
+                        <Mail className="icon" />
+                        <span>Sransonette@gmail.com</span>
                     </div>
                 </div>
                 <div className="right"></div>
