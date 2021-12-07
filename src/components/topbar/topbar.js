335116c96da4ac55eded8e0e2ddde87b18550d8a
Sean <Sransonette@gmail.com>
2021-12-06 22:30:01 -0600

    active added in topbar div and scss added in topbar.scss to change colors of top bar

--- src/components/topbar/topbar.js
+++ src/components/topbar/topbar.js
@@ -4,7 +4,7 @@ import { Person, Mail } from "@material-ui/icons"
 
 const Topbar = () => {
     return (
-        <div className="topbar">
+        <div className="topbar active">
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Sean.</a>
